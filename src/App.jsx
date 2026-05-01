bdd9b68175e924ad5168009712aaa20067c8ed5d
Kleo Fili <kleofili60@gmail.com>
2026-05-01 12:00:11 +0200

    fix overflow

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
-import Pricing from "./components/Pricing/Pricing";
+import Pricing from "./components/Pricing/Pricing"; 
 import Team from "./components/Team/Team";
 import Blog from "./components/Blog/Blog";
 import Contact from "./components/ContactForm/ContactForm";
