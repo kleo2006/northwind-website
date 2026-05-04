71bb8b194114ab3ea1c327bde64a9aa072d7f154
Kleo Fili <kleofili60@gmail.com>
2026-05-04 18:11:39 +0200

    Fix production bootstrap for Cloudflare Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,37 @@
-import "@vitejs/plugin-react/preamble";
+// import "@vitejs/plugin-react/preamble";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import { ThemeProvider } from "./context/ThemeContext";
+// import "./styles/global.css";
+// import App from "./App";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <ThemeProvider>
+//         <App />
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./styles/global.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>
 );
 
